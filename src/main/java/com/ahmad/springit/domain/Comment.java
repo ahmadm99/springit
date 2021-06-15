841d60b4ef7c7e7364edc2e597e5f560b5e017ed
ahmadm99 <ahmad.m.mansour99@gmail.com>
2021-06-15 16:40:26 +0300

    Added mapping to Link -> Comment, fixed some data types, annotated @NonNull in Link.java

--- src/main/java/com/ahmad/springit/domain/Comment.java
+++ src/main/java/com/ahmad/springit/domain/Comment.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.util.Objects;
 
 @Entity
@@ -14,10 +15,12 @@ import java.util.Objects;
 public class Comment {
     @Id
     @GeneratedValue
-    private long id;
+    private Long id;
     private String body;
 
     //link
+    @ManyToOne
+    private Link link;
 
 
 }
--- src/main/java/com/ahmad/springit/domain/Link.java
+++ src/main/java/com/ahmad/springit/domain/Link.java
@@ -2,10 +2,14 @@ package com.ahmad.springit.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -16,10 +20,14 @@ public class Link {
 
     @Id
     @GeneratedValue
-    private long id;
+    private Long id;
+    @NonNull
     private String title;
+    @NonNull
     private String url;
 
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
     //comments
 
 }
