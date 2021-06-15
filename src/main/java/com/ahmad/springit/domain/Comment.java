e8d96cf9dc8e95faa90678d597dfaeef2f88b064
ahmadm99 <ahmad.m.mansour99@gmail.com>
2021-06-15 15:26:27 +0300

    Created our entities and refactored using Project Lombok

---
+++ src/main/java/com/ahmad/springit/domain/Comment.java
@@ -0,0 +1,23 @@
+package com.ahmad.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String body;
+
+    //link
+
+
+}
---
+++ src/main/java/com/ahmad/springit/domain/Link.java
@@ -0,0 +1,25 @@
+package com.ahmad.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Link {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String title;
+    private String url;
+
+    //comments
+
+}
