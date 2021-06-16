e4f9aeb7cdc7d55a15c3ed2fb02bc50c73b755ff
ahmadm99 <ahmad.m.mansour99@gmail.com>
2021-06-16 21:55:47 +0300

    Created LinkController and a prototype HomeController

---
+++ src/main/java/com/ahmad/springit/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.ahmad.springit.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/") //This annotation is the same as @RequestMapping(path = "/", RequestMethod.Get) not sure on syntax
+    public String homePage(){
+        return "Hello, this is my homepage - brought to you by the Controller";
+    }
+}
