c74d6f392b4f9cc6b8dca5c0c8f0c199b44e1b6d
ahmadm99 <ahmad.m.mansour99@gmail.com>
2021-06-22 12:42:35 +0300

    Added account register and profile mappings and templates

--- src/main/java/com/ahmad/springit/controller/AuthController.java
+++ src/main/java/com/ahmad/springit/controller/AuthController.java
@@ -9,4 +9,13 @@ public class AuthController {
     public String login(){
         return "auth/login";
     }
+    @GetMapping("/profile")
+    public String profile() {
+        return "auth/profile";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "auth/register";
+    }
 }
