94d3e0a12208e2293051b5b81d45a5fdf280921c
ahmadm99 <ahmad.m.mansour99@gmail.com>
2021-06-28 22:27:33 +0300

    We now display aliases on links instead of user emails

--- src/main/java/com/ahmad/springit/Bootstrap/DatabaseLoader.java
+++ src/main/java/com/ahmad/springit/Bootstrap/DatabaseLoader.java
@@ -20,13 +20,12 @@ import java.util.Map;
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
-
     private LinkRepository linkRepository;
     private CommentRepository commentRepository;
     private UserRepository userRepository;
     private RoleRepository roleRepository;
-    //Spring autowires these through constructor
 
+    private Map<String,User> users = new HashMap<>();
 
     public DatabaseLoader(LinkRepository linkRepository, CommentRepository commentRepository, UserRepository userRepository, RoleRepository roleRepository) {
         this.linkRepository = linkRepository;
@@ -37,7 +36,10 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+
+        // add users and roles
         addUsersAndRoles();
+
         Map<String,String> links = new HashMap<>();
         links.put("Securing Spring Boot APIs and SPAs with OAuth 2.0","https://auth0.com/blog/securing-spring-boot-apis-and-spas-with-oauth2/?utm_source=reddit&utm_medium=sc&utm_campaign=springboot_spa_securing");
         links.put("Easy way to detect Device in Java Web Application using Spring Mobile - Source code to download from GitHub","https://www.opencodez.com/java/device-detection-using-spring-mobile.htm");
@@ -52,7 +54,15 @@ public class DatabaseLoader implements CommandLineRunner {
         links.put("File download example using Spring REST Controller","https://www.jeejava.com/file-download-example-using-spring-rest-controller/");
 
         links.forEach((k,v) -> {
+            User u1 = users.get("user@gmail.com");
+            User u2 = users.get("super@gmail.com");
             Link link = new Link(k,v);
+            if(k.startsWith("Build")) {
+                link.setUser(u1);
+            } else {
+                link.setUser(u2);
+            }
+
             linkRepository.save(link);
 
             // we will do something with comments later
@@ -79,18 +89,21 @@ public class DatabaseLoader implements CommandLineRunner {
         Role adminRole = new Role("ROLE_ADMIN");
         roleRepository.save(adminRole);
 
-        User user = new User("user@gmail.com",secret,true);
+        User user = new User("user@gmail.com",secret,true,"Joe","User","joedirt");
         user.addRole(userRole);
         userRepository.save(user);
+        users.put("user@gmail.com",user);
 
-        User admin = new User("admin@gmail.com",secret,true);
+        User admin = new User("admin@gmail.com",secret,true,"Joe","Admin","masteradmin");
+        admin.setAlias("joeadmin");
         admin.addRole(adminRole);
         userRepository.save(admin);
+        users.put("admin@gmail.com",admin);
 
-        User master = new User("master@gmail.com",secret,true);
+        User master = new User("super@gmail.com",secret,true,"Super","User","superduper");
         master.addRoles(new HashSet<>(Arrays.asList(userRole,adminRole)));
         userRepository.save(master);
-
+        users.put("super@gmail.com",master);
     }
 
 }
