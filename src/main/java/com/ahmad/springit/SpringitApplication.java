ac5787091ffd61afcd674dbe9076792decbc9406
ahmadm99 <ahmad.m.mansour99@gmail.com>
2021-06-29 15:16:03 +0300

    Started production branch, haven't done anything yet

--- src/main/java/com/ahmad/springit/SpringitApplication.java
+++ src/main/java/com/ahmad/springit/SpringitApplication.java
@@ -1,16 +1,9 @@
 package com.ahmad.springit;
 
-import com.ahmad.springit.domain.Comment;
-import com.ahmad.springit.domain.Link;
-import com.ahmad.springit.repository.CommentRepository;
-import com.ahmad.springit.repository.LinkRepository;
 import org.ocpsoft.prettytime.PrettyTime;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
